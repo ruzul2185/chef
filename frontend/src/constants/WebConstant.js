c0c67c66c8c074acbc1b29e2d12efa0fe3dcc9d5
ruzul <ruzulchaure2185@gmail.com>
2024-06-18 23:31:20 +0530

    categories added

--- frontend/src/constants/WebConstant.js
+++ frontend/src/constants/WebConstant.js
@@ -1,7 +1,7 @@
 export const DropDownListOne = [
     {
         id:1,
-      name:"Hot Sets",
+        name:"Hot Sets",
         href:"hot-sets",
     },
     {
@@ -21,7 +21,7 @@ export const DropDownListOne = [
     },
     {
         id:5,
-        name:"Grill Paris",
+        name:"Grill Pans",
         href:"grill-pans",
     },
     {
@@ -75,346 +75,106 @@ export const DropDownListOne = [
         href:"combos",
     },
     {
-        id:16,
-        name:"Royal Velvet Non-stick",
-        href:"royal-velvet",
-    },
-    {
-        id:17,
-        name:"Forza Cast-iron",
-        href:"forza-cast-iron",
-    },
-    {
-        id:18,
-        name:"Ferro Cast-iron",
-        href:"ferro-cast-iron",
-    },
-    {
-        id:19,
-        name:"Caesar Die-case",
-        href:"caesar",
-    },
-    {
-        id:20,
-        name:"Ebony Hard Anodized",
-        href:"ebony",
-    },
-    {
-        id:21,
-        name:"Granite Non-stick",
-        href:"granite",
-    },
-    {
-        id:22,
-        name:"Tri-Ply Stainless Steel",
-        href:"tri-ply-stainless-steel",
-    },
-    {
-        id:23,
-        name:"Graphite Non-stick",
-        href:"graphite-non-stick",
-    },
-    {
-        id:24,
-        name:"Ceramic",
-        href:"ceramic",
-    },
-    {
-        id:25,
-        name:"Duralite Die-cast",
-        href:"duralife-die-cast",
-    },
-    {
-        id:26,
-        name:"Non-stick",
-        href:"non-stick",
-    },
-    {
-        id:27,
-        name:"Hard Anodized",
-        href:"ebony",
-    },
-    {
         id:28,
-        name:"Tri-Ply Steel",
-        href:"tri-ply-stainless-steel",
+        name:"Gas Toaster & Griller",
+        href:"gas-toaster-griller",
     },
     {
         id:29,
-        name:"Cast-iron",
-        href:"cast-iron",
+        name:"Cookware sets",
+        href:"cookware-sets",
     },
 ];
 
 export const DropDownListTwo = [
     {
-      id:1,
-      name:"Kitchen Cooling Fan",
-        href:"symphony-duet-i-kitchen-cooling-fan",
+        id:1,
+        name:"OTG",
+        href:"otg",
     },
     {
         id:2,
-        name:"Magneto",
-        href:"magneto",
+        name:"Microwave Oven",
+        href:"microwave-oven",
     },
     {
         id:3,
-        name:"Roti-Magic",
-        href:"roti-magic",
-    },
-    {
-        id:4,
-        name:"Crimson Edge Collection",
-        href:"crimson-edge-premium-collection",
-    },
-    {
-        id:5,
-        name:"Baking Essentials",
-        href:"baking-essentials",
-    },
-    {
-        id:6,
-        name:"Microwave",
-        href:"microwave",
-    },
-    {
-        id:7,
-        name:"OTGs",
-        href:"otgs",
-    },
-    {
-        id:8,
-        name:"Hand Mixers & Blenders",
-        href:"mixers-blenders",
-    },
-    {
-        id:9,
-        name:"Sandwich Makers",
-        href:"sandwich-makers",
-    },
-    {
-        id:10,
-        name:"Kettles",
-        href:"electric-kettles",
-    },
-    {
-        id:11,
-        name:"Coffee Machines",
-        href:"coffee-machine",
-    },
-    {
-        id:12,
-        name:"Toasters",
-        href:"pop-up-toasters",
-    },
-    {
-        id:13,
-        name:"Sanjeev Kapoor Tandoor",
-        href:"sanjeev-kapoor-tandoor",
-    },
-    {
-        id:14,
-        name:"Grills",
-        href:"grills-barbeque-tandoor",
-    },
-    {
-        id:15,
-        name:"Egg Boiler",
-        href:"egg-boiler",
-    },
-    {
-        id:16,
-        name:"Air Fryers",
-        href:"air-fryers",
-    },
-    {
-        id:17,
-        name:"BBQ",
-        href:"smoky-grill-electric-barbeque-1650w-1-year-warranty",
-    },
-    {
-        id:18,
-        name:"Waffle Maker",
-        href:"waffle-maker",
-    },
-    {
-        id:19,
-        name:"Electric Dosa Tawa",
-        href:"electric-dosa-tawa",
-    },
-    {
-        id:20,
-        name:"Cold Press Juicer",
-        href:"cold-press-juicer",
-    },
-    {
-        id:21,
-        name:"Juicers",
-        href:"juicers",
-    },
-    {
-        id:22,
-        name:"Soup Maker",
-        href:"soup-maker",
-    },
-    {
-        id:23,
-        name:"Mixer Grinders",
-        href:"mixers-grinder",
-    },
-    {
-        id:24,
-        name:"Nutri-Blend",
-        href:"nutri-blends-all",
-    },
-    {
-        id:25,
-        name:"Nutri-Pot",
-        href:"nutripot",
-    },
-    {
-        id:26,
-        name:"Electric Choppers",
-        href:"electric-choppers",
-    },
-    {
-        id:27,
-        name:"Stand Mixers",
-        href:"stand-mixers",
-    },
-    {
-        id:28,
-        name:"Rice Cookers",
-        href:"nutri-cook-rice-cooker-with-single-bowl-1-8-litres-2-years-warranty",
-    },
-    {
-        id:29,
-        name:"Induction Cooktops",
-        href:"induction-cooktops",
-    },
-    {
-        id:30,
-        name:"Wet Grinder",
-        href:"wonderchef-maxima-wet-grinder-2-l",
-    },
-    {
-        id:31,
-        name:"Water Heaters",
-        href:"water-heaters",
-    },
-    {
-        id:32,
-        name:"Irons",
-        href:"irons",
-    },
-    {
-        id:33,
-        name:"Combos",
-        href:"combos",
-    },
-    {
-        id:34,
-        name:"Bread Maker",
-        href:"regalia-atta-kneader-bread-maker",
-    },
-    {
-        id:35,
-        name:"Nutri-Cup",
-        href:"nutri-cup",
+        name:"Griller",
+        href:"griller",
     },
 ];
 
 export const DropDownListThree = [
     {
-      id:1,
-      name:"Nutri-Blend Smart",
-        href:"nutri-blend-smart",
+        id:1,
+        name:"Hand Blender",
+        href:"hand-blender",
     },
     {
         id:2,
-        name:"Nutri-Blend Go",
-        href:"nutri-blend-go",
+        name:"Hand Mixer",
+        href:"hand-mixer",
     },
     {
         id:3,
-        name:"Nutri-Blend BOLT",
-        href:"nutri-blend-bolt",
+        name:"Mixer Grinder",
+        href:"mixer-grinder",
     },
     {
         id:4,
-        name:"Nutri-Blend 2 Jars",
-        href:"nutriblend-mixer-grinder-2-jar",
+        name:"Juicer Mixer Grinder",
+        href:"juicer-mixer-grinder",
     },
     {
         id:5,
-        name:"Nutri-Blend 3 Jars",
-        href:"nutriblend-mixer-grinder-3-jar",
+        name:"Juicer",
+        href:"juicer",
     },
     {
         id:6,
-        name:"Nutri-Blend Complete Kitchen Machine",
-        href:"nutriblend-complete-kitchen-machine",
+        name:"Nutri-Blend",
+        href:"nutri-blend",
     },
     {
         id:7,
-        name:"Nutri-Blend Compact Food Processor",
-        href:"nutriblend-food-processor",
+        name:"Food Processor",
+        href:"food-processor",
     },
     {
         id:8,
-        name:"Nutri-Blend Thunder",
-        href:"nutri-blend-thunder-1000w",
-    },
-    {
-        id:9,
-        name:"Nutri-Blend Personal",
-        href:"wonderchef-nutri-blend-personal",
-    },
-    {
-        id:10,
-        name:"Nutri-Blend Spare Parts",
-        href:"wonderchef-nutri-blend-personal",
+        name:"Roti Maker",
+        href:"roti-maker",
     },
 ];
 
 export const DropDownListFour = [
     {
-      id:1,
-      name:"Inner Lid",
-        href:"inner-lid",
+        id:1,
+        name:"Sandwich Maker",
+        href:"sandwich-maker",
     },
     {
         id:2,
-        name:"Outer Lid",
-        href:"outer-lid",
-    },
-    {
-        id:3,
-        name:"Specialty Cookers",
-        href:"specialty-cookers",
-    },
-    {
-        id:4,
-        name:"Combo",
-        href:"combo",
+        name:"Toaster",
+        href:"toaster",
     },
 ];
 
 export const DropDownListFive = [
     {
       id:1,
-      name:"Gas Cooktops",
-        href:"gas-cooktops",
+      name:"Coffee Machine",
+        href:"coffee-machine",
     },
     {
         id:2,
-        name:"Induction Cooktops",
-        href:"induction-cooktops",
+        name:"Tea Machine",
+        href:"tea-machine",
     },
     {
         id:3,
-        name:"Infrared Cooktops",
-        href:"infrared-cooktop",
+        name:"Kettle",
+        href:"kettle",
     },
 ];
 
