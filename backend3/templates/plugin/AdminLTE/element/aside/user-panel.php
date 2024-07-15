5132f1299d4736e7bd02092a71dc418fbe89e971
Amol Sartajsingh Maskare <amolmaskare1@gmail.com>
2024-07-15 17:12:51 +0530

    remove backend

--- backend3/templates/plugin/AdminLTE/element/aside/user-panel.php
+++
@@ -1,9 +0,0 @@
-<div class="user-panel">
-    <div class="pull-left image">
-        <?php echo $this->Html->image('user2-160x160.jpg', array('class' => 'img-circle', 'alt' => 'User Image')); ?>
-    </div>
-    <div class="pull-left info">
-        <p>Sathe's Metalwares</p>
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-    </div>
-</div>
--- frontend/src/constants/URLConstant.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'http://localhost:8765'; 
-
-export const AUTHENTICATE_URL = BASE_URL + '/api/Items/getItems';
-export const GET_PRODUCT_LIST = BASE_URL + '/api/Products/getProductLists';
-export const GET_CATEGORY_LIST = BASE_URL + '/api/Categories/getCategories';
-export const GET_PRODUCT_DETAIL = BASE_URL + '/api/Products/getProductDetail';
-export const GET_LATEST_PRODUCTS = BASE_URL + '/api/Products/getLatestProducts';
-export const URL = 'http://localhost:8765/users/login';
-
