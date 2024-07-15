8d62a9d760e3c1fba5f74c5a91c21df0c4588621
ruzul2185 <136430323+ruzul2185@users.noreply.github.com>
2024-07-15 17:47:34 +0530

    Update URLConstant.js
    new frontend

--- frontend/src/constants/URLConstant.js
+++ frontend/src/constants/URLConstant.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8765'; 
+// const BASE_URL = 'http://localhost:8765'; 
+const BASE_URL = 'https://majesticstylist.000webhostapp.com'; 
 
 export const AUTHENTICATE_URL = BASE_URL + '/api/Items/getItems';
 export const GET_PRODUCT_LIST = BASE_URL + '/api/Products/getProductLists';
