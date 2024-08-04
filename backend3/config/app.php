7e0dbcb2266f43d36814e6c6a1a0364092ab9183
PriyaBaniya1 <baniyapriya111@gmail.com>
2024-08-04 19:59:10 +0530

    url constanct change, new photos add, security salt added

--- backend3/config/app.php
+++ backend3/config/app.php
@@ -17,7 +17,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
 
     /*
      * Configure basic information about the application.
@@ -76,7 +76,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT'),
+        'salt' => env('SECURITY_SALT','f68f969fd9a697772eaafd74d380b1c02ae680ad52cb539a4f0589c293cc81f4'),
     ],
 
     /*
--- frontend/src/constants/URLConstant.js
+++ frontend/src/constants/URLConstant.js
@@ -1,9 +1,9 @@
-// const BASE_URL = 'http://localhost:8765'; 
-const BASE_URL = 'https://majesticstylist.000webhostapp.com'; 
+const BASE_URL = 'http://localhost:8765'; 
+// const BASE_URL = 'https://majesticstylist.000webhostapp.com'; 
 
 export const AUTHENTICATE_URL = BASE_URL + '/api/Items/getItems';
 export const GET_PRODUCT_LIST = BASE_URL + '/api/Products/getProductLists';
-export const GET_CATEGORY_LIST = '/api/Categories/getCategories';
+export const GET_CATEGORY_LIST = BASE_URL + '/api/Categories/getCategories';
 export const GET_PRODUCT_DETAIL = BASE_URL + '/api/Products/getProductDetail';
 export const GET_LATEST_PRODUCTS = BASE_URL + '/api/Products/getLatestProducts';
 export const URL = 'http://localhost:8765/users/login';
