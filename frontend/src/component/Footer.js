d675a6800dbebbf5b66cfcb7c6153e3040e0b942
PriyaBaniya1 <baniyapriya111@gmail.com>
2024-08-05 15:45:57 +0530

    Footer changes add / new footer

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,69 +1,66 @@
-import React from 'react';
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-    return(
-        <React.Fragment>
-            <div className={styles.mainContainer}>
-                <div>
-                    <div className={styles.heading}>
-                        SUBSCRIBE TO OUR NEWSLETTER
-                    </div>
-                    <div className={styles.description}>
-                        Subscribe to email alerts. We promise not to spam your inbox.
-                    </div>
-                    <div className={styles.emailContainer}>
-                        <input className={styles.inputContainer} type={"text"} placeholder={"Enter Email"}/>
-                        <button className={styles.submitButton} type={"submit"}>SUBSCRIBE</button>
-                    </div>
-                </div>
-                <div className={styles.optionContainer}>
-                    <div className={styles.heading}>
-                        ABOUT Sathe Metal's
-                    </div>
-                    <a href="../pages/about-us" className={styles.anchor}>About Us</a>
-                    <a href="#" className={styles.anchor}>Investors</a>
-                </div>
-                <div className={styles.optionContainer}>
-                    <div className={styles.heading}>
-                        OUR POLICIES
-                    </div>
-                    <a href="#" className={styles.anchor}>Return Policy</a>
-                    <a href="#" className={styles.anchor}>Terms & Conditions</a>
-                    <a href="#" className={styles.anchor}>Privacy Policy</a>
-                    <a href="#" className={styles.anchor}>Wonderpoints T&C</a>
-                </div>
-                <div className={styles.optionContainer}>
-                    <div className={styles.heading}>
-                        INFORMATION
-                    </div>
-                    <a href="#" className={styles.anchor}>E-Recipe Books</a>
-                    <a href="#" className={styles.anchor}>Blogs & Recipes</a>
-                    <a href="#" className={styles.anchor}>All Blogs</a>
-                </div>
-                <div className={styles.optionContainer}>
-                    <div className={styles.heading}>
-                        NEED HELP?
-                    </div>
-                    <a href="#" className={styles.anchor}>Track Your Order</a>
-                    <a href="#" className={styles.anchor}>Online Service Request</a>
-                    <a href="#" className={styles.anchor}>Warranty Registration</a>
-                    <a href="#" className={styles.anchor}>Replacement Request</a>
-                    <a href="#" className={styles.anchor}>Gift Card</a>
-                    <a href="../pages/contact-us" className={styles.anchor}>Contact Us</a>
-                </div>
-                <div className={styles.optionContainer}>
-                    <div className={styles.heading}>
-                        OTHER LINKS
-                    </div>
-                    <a href="#" className={styles.anchor}>DTH Member Login</a>
-                    <a href="#" className={styles.anchor}>Locate Brand Outlets</a>
-                    <a href="#" className={styles.anchor}>Franchise Opportunity</a>
-                    <a href="#" className={styles.anchor}>Corporate Gifting</a>
-                </div>
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.section}>
+        <div className={styles.heading}>SUBSCRIBE TO OUR NEWSLETTER</div>
+        <div className={styles.description}>
+          Subscribe to email alerts. We promise not to spam your inbox.
+        </div>
+        <div className={styles.emailContainer}>
+          <input
+            className={styles.inputContainer}
+            type="text"
+            placeholder="Enter Email"
+          />
+          <button className={styles.submitButton} type="submit">
+            SUBSCRIBE
+          </button>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.heading}>Find Us Here</div>
+        <div className={styles.address}>
+        Sathe's Metalwares West High Court Road, Gokulpeth, Gokulpeth,
+        opposite Rama Theator, Nagpur, Maharashtra 440010
+        </div>
+        
+      </div>
+      <div className={styles.section}>
+        <div className={styles.heading}>Contact Us</div>
+        <div className={styles.contactDetails}>
+          <a href="tel:+917122533308" className={styles.anchor}>
+            +917122533308
+          </a>
+          <br />
+          <a href="mailto:sathemetalwares@gmail.com" className={styles.anchor}>
+            sathemetalwares@gmail.com
+          </a>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.heading}>About Us</div>
+        <div className={styles.address}>
+        Established in 1974 by Chintamani Sathe and 
+        now led by Pardeep S. Sathe....
+        <a href="../pages/about-us" className={styles.anchor}>Read More</a>
+        </div>
+        <div className={styles.socialMedia}>
+          <a href="#" className={styles.socialIcon}><FontAwesomeIcon icon={faFacebook} /></a>
+          <a href="#" className={styles.socialIcon}><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href="#" className={styles.socialIcon}><FontAwesomeIcon icon={faInstagram} /></a>
+          <a href="#" className={styles.socialIcon}><FontAwesomeIcon icon={faLinkedin} /></a>
+        </div>
+      </div>
+      <div className={styles.footerBottom}>
+        Created By Priya Baniya | All Rights Reserved!
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
