0648a200f8ff581e52546a6f18c90e710ca5dfdf
PriyaBaniya1 <baniyapriya111@gmail.com>
2024-08-08 20:41:50 +0530

    add code in new arrival in mainpage data and no data found.. error message also added

--- frontend/src/page/MainPage.js
+++ frontend/src/page/MainPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from "../component/Header";
 import HaloSection from "../component/HaloSection";
 import bestSellers from "../asset/Bestseller.png";
@@ -15,72 +16,88 @@ import Review from "../component/Review";
 import OutletBanner from "../component/OutletBanner";
 import Footer from "../component/Footer";
 import LoadingOverlay from 'react-loading-overlay';
-import PacmanLoader from 'react-spinners/PacmanLoader'
-
+import PacmanLoader from 'react-spinners/PacmanLoader';
+import { getLatestProducts } from '../stores/actions/auth'; // Assuming this action fetches the products
 
 const MainPage = () => {
-    
     const [isActive, setIsActive] = useState(false);
-    // useEffect(()=>{
-    //     setIsActive(false);
-    // },[])
-    return(
+    const dispatch = useDispatch();
+
+    // Fetch the latest products to populate categoryList
+    useEffect(() => {
+        dispatch(getLatestProducts());
+    }, [dispatch]);
+
+    // Access the categoryList from Redux state
+    const categoryList = useSelector(state => state.auth.LatestProducts);
+
+    return (
         <div>
             <LoadingOverlay
                 active={isActive}
                 spinner={<PacmanLoader />}
-                style = {{paddingTop:'20px'}}
-                // text='Loading your content...'
-                >
-            {/*<Header/>*/}
-            {/*<img alt="hare" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>*/}
-            <div className={classes.haloContainer}>
-                <div className={classes.haloSection}>
-                    <HaloSection link={"/collections/best-selling-product"} image={bestSellers} title={"BEST SELLERS"}/>
-                    <HaloSection link={"/collections/cookware"} image={cookware} title={"COOKWARE"}/>
-                    <HaloSection link={"/collections/appliances"} image={applicance} title={"APPLIANCES"}/>
-                    {/* <HaloSection link={"/collections/spares-accessories"} image={"//www.wonderchef.com/cdn/shop/files/Spare-Parts_160x.png?v=1702034021"} title={"SPARE PARTS"}/> */}
-                    <HaloSection link={"/collections/new-arrivals"} image={newArrival} title={"NEW ARRIVALS"}/>
-                    {/* <HaloSection link={"/collections/offers"} image={"//www.wonderchef.com/cdn/shop/files/Deals_74fb3874-a0c5-4ab7-aac2-a34ee0f6686a_160x.png?v=1715148593"} title={"OFFERS"}/> */}
+                style={{ paddingTop: '20px' }}
+            >
+                {/* Rest of your components */}
+                <div className={classes.haloContainer}>
+                    <div className={classes.haloSection}>
+                        <HaloSection 
+                            link="/collections/best-selling-product" 
+                            image={bestSellers} 
+                            title="BEST SELLERS" 
+                            categoryList={categoryList}
+                            noDataMessage="No data found. Please add some data in BEST SELLERS."
+                        />
+                        <HaloSection 
+                            link="/category/cookware" 
+                            image={cookware} 
+                            title="COOKWARE" 
+                            categoryList={categoryList}
+                            noDataMessage="No data found. Please add some data in COOKWARE."
+                        />
+                        <HaloSection 
+                            link="/category/Kitchen Appliances" 
+                            image={applicance} 
+                            title="APPLIANCES" 
+                            categoryList={categoryList}
+                            noDataMessage="No data found. Please add some data in APPLIANCES."
+                        />
+                        <HaloSection 
+                            link="/all-products" 
+                            image={newArrival} 
+                            title="NEW ARRIVALS" 
+                            categoryList={categoryList}
+                            noDataMessage="No data found. Please add some data in NEW ARRIVALS."
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div className={classes.slideShowContainer}>
-                <SlideShow />
-            </div>
+                <div className={classes.slideShowContainer}>
+                    <SlideShow />
+                </div>
 
-            {/* <div className={classes.productCarouselConatiner}> */}
-            <div className={`${classes.productCarouselConatiner} ${classes.container}`}>
+                <div className={`${classes.productCarouselConatiner} ${classes.container}`}>
+                    <Product title="NEW ARRIVALS" />
+                </div>
 
-                <Product title={"NEW ARRIVALS"}/>
-            </div>
+                <div>
+                    <CategoryComponent />
+                </div>
 
-            {/* <div>
-                <Jumbotron />
-            </div> */}
+                <div className={classes.container}>
+                    <Review title="HAPPY CUSTOMERS" />
+                </div>
 
-            <div>
-                <CategoryComponent />
-            </div>
+                <div className={classes.productCarouselConatiner}>
+                    <Product title="OUR BEST SELLERS" />
+                </div>
 
-            {/* <div className={classes.productCarouselConatiner}>
-                <Product title={"SUMMERTIME SELECTION"}/>
-            </div> */}
-
-            <div className={classes.container}>
-                <Review title={"HAPPY CUSTOMERS"}/>
-            </div>
-
-            <div className={classes.productCarouselConatiner}>
-                <Product title={"OUR BEST SELLERS"}/>
-            </div>
-
-            <div>
-                <OutletBanner title={"AVAILABLE AT OVER 10,000 OUTLETS INCLUDING ALL DEPARTMENT STORES"}/>
-            </div>
+                <div>
+                    <OutletBanner title="AVAILABLE AT OVER 10,000 OUTLETS INCLUDING ALL DEPARTMENT STORES" />
+                </div>
             </LoadingOverlay>
         </div>
-    )
-}
+    );
+};
 
 export default MainPage;
